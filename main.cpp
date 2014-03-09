0bc6578b90f36f9019395560a50600f1d02eb854
olya-d <inremajor@gmail.com>
2014-03-09 17:24:09 +0100

    define the constants
    define number of faces, samples, resolution

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+
+const int Faces = 2;
+const int Samples = 1;
+const int Width = 92;
+const int Height = 112;
+const string Data_path = "/Users/olga_andreyeva/Dropbox/Developer/eigenfaces_c/faces_ascii/";
+const int N = Faces*Samples; // Total number of images
+const int M = Width*Height; // Resolution
+
+
+int main(int argc, const char * argv[])
+{
+    Matrix images;
+
+    return 0;
+}
+
